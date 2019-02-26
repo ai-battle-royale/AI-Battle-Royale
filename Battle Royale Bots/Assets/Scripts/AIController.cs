7cc174a82bc2c9eb751d83a2dc9e0e09df23f7cf
Blieper <yoranmandema@eml.cc>
2019-02-26 14:49:40 +0100

    Scan() + Move() + Stop() + AIController properties

---
+++ Battle Royale Bots/Assets/Scripts/AIController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct ScanInfo {
+    public float Distance;
+    public HitType Type;
+
+    public ScanInfo (float d, HitType t) {
+        Distance = d;
+        Type = t;
+    }
+}
+
+public enum HitType {
+    None,
+    Enemy,
+    Item,
+    World
+}
+
+[RequireComponent(typeof(CharacterController))]
+public class AIController : MonoBehaviour {
+
+    // These constants should be moved over to a game manager
+    public float MaxLookDistance = 5f;
+    public float MoveSpeed = 1f;
+    public LayerMask DefaultLayerMask;
+
+    public float    Health     { get; private set; } = 100f;
+    public float    Armor      { get; private set; } = 0f;
+    public bool     IsMoving   { get; private set; } = false;
+
+    private CharacterController characterController;
+    private float moveAngle;
+
+    void Start() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+        characterController.Move(GetDirectionFromAngle(moveAngle) * MoveSpeed * Time.deltaTime);
+    }
+
+    // There's probably already a built-in function for this
+    Vector3 GetDirectionFromAngle (float angle) {
+        return new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle));
+    }
+    
+    public ScanInfo Scan (float angle, LayerMask mask = default) {
+
+        if (mask == default) mask = DefaultLayerMask;
+
+        if (Physics.Raycast(transform.position, GetDirectionFromAngle(angle), out RaycastHit hit, MaxLookDistance, mask)) {
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+
+            return new ScanInfo(hit.distance, HitType.World);
+        }
+        else {
+            Debug.DrawLine(transform.position, transform.position + GetDirectionFromAngle(angle) * MaxLookDistance, Color.green);
+
+            return new ScanInfo(MaxLookDistance, HitType.None);
+        }
+    }
+
+    public void Move (float angle) {
+        IsMoving = true;
+        moveAngle = angle;
+    }
+
+    public void Stop() {
+        IsMoving = false;
+    }
+}
---
+++ Battle Royale Bots/Assets/Scripts/AITest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AITest : MonoBehaviour
+{
+
+    AIController Controller;
+
+    void Start()  {
+        Controller = GetComponent<AIController>();
+    }
+
+    void Update() {       
+        for (var i = 0f; i < Mathf.PI * 2; i += Mathf.PI/10) {
+            Controller.Scan(i);
+        }
+
+        Controller.Move(45);
+    }
+}
