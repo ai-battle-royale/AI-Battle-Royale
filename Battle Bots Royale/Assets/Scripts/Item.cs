09d9925e793401e5c35047dead2811f7ca591d10
Blieper <yoranmandema@eml.cc>
2019-03-01 17:46:43 +0100

    Medkit test item

--- Battle Bots Royale/Assets/Scripts/Item.cs
+++ Battle Bots Royale/Assets/Scripts/Item.cs
@@ -7,8 +7,11 @@ public abstract class Item : ScriptableObject
     public GameObject Owner;
     public BattleBotInterface Controller;
 
-    public abstract float Amount { get; }
     public abstract float ConsumptionTime { get; }
+
+    public bool IsBeingUsed { get; private set; }
+
+    private float useTime;
 
     public static T Instantiate<T>(GameObject owner) where T : Item
     {
@@ -20,7 +23,32 @@ public abstract class Item : ScriptableObject
         return item;
     }
 
+    private void Awake() {
+        // Ensure that the item can be used as soon as it's spawned.
+        useTime = Time.time - ConsumptionTime;
+    }
 
+    private void Update () {
+        if (IsBeingUsed) {
+            if ((Time.time - useTime) >= ConsumptionTime) {
+                Debug.Log($"Used item {this}");
 
-    private abstract void OnUse();
+                OnUse();
+
+                Destroy(this);
+            }
+        }
+    }
+
+    public void Cancel () {
+        IsBeingUsed = false;
+    }
+
+    public void Use () {
+        IsBeingUsed = true;
+
+        useTime = Time.time;
+    }
+
+    public abstract void OnUse();
 }
---
+++ Battle Bots Royale/Assets/Scripts/ItemMedkit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMedkit : Item {
+    public float Amount => 50f;
+    public override float ConsumptionTime => 3f;
+
+    public override void OnUse() {
+        Controller.Health += 100 - Amount;
+    }
+}
