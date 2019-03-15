a2efe434389566aa2516f23b8be0828831f2bb35
GSjoerdstra <45204487+GSjoerdstra@users.noreply.github.com>
2019-03-15 20:22:38 +0100

    Update Geert.cs
    Should be working

--- Battle Bots Royale/Assets/Scripts/AI/Geert.cs
+++ Battle Bots Royale/Assets/Scripts/AI/Geert.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Geert : MonoBehaviour
 {
-    BattleBotInterface BBInterface;
+    BattleBotInterface BotInterface;
     private Vector3 direction;
+    Pickup pickupTarget;
     // Start is called before the first frame update
     void Start()
     {
-        BBInterface = GetComponent<BattleBotInterface>();
+        BotInterface = GetComponent<BattleBotInterface>();
         direction = new Vector3(Random.value, 0, Random.value);
     }
 
@@ -19,25 +20,30 @@ public class Geert : MonoBehaviour
         for (var i = 0f; i < Mathf.PI * 2; i += Mathf.PI / 8)
         {
             var dir = new Vector3(Mathf.Cos(i), 0, Mathf.Sin(i));
-            var scan = BBInterface.Scan(dir);
+            var scan = BotInterface.Scan(dir);
 
             if (scan.type == HitType.World)
             {
-
+                direction = Vector3.Slerp(direction, -dir, 1 - (scan.distance / GameManager.instance.maxLookDistance));
             }
             else if (scan.type == HitType.Enemy)
             {
-                BBInterface.Shoot(dir);
+                BotInterface.Shoot(dir);
+                direction = scan.distance > 2f ? dir : -dir;
+
+                break;
             }
             else if (scan.type == HitType.Item)
             {
+                pickupTarget = scan.pickup;
 
+                direction = (pickupTarget.transform.position - transform.position).normalized;
             }
 
         }
 
 
-        BBInterface.Move(direction);
+        BotInterface.Move(direction);
     }
 
 }
