8b91917651b916e811c38be45eb236ab80ed155b
GSjoerdstra <45204487+GSjoerdstra@users.noreply.github.com>
2019-03-15 22:47:26 +0100

    Merge branch 'geert-bot'

--- Battle Bots Royale/Assets/Scripts/AI/Geert.cs
+++ Battle Bots Royale/Assets/Scripts/AI/Geert.cs
@@ -4,24 +4,60 @@ using UnityEngine;
 
 public class Geert : MonoBehaviour
 {
-    BattleBotInterface BBInterface;
+    BattleBotInterface BotInterface;
     private Vector3 direction;
+    Pickup pickupTarget;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        BBInterface = GetComponent<BattleBotInterface>();
+        BotInterface = GetComponent<BattleBotInterface>();
         direction = new Vector3(Random.value, 0, Random.value);
     }
 
     // Update is called once per frame
     void Update()
     {
+        var healingItem = BotInterface.FindItem<HealingItem>();
+        var armorItem = BotInterface.FindItem<ArmorItem>();
+
         for (var i = 0f; i < Mathf.PI * 2; i += Mathf.PI / 8)
         {
             var dir = new Vector3(Mathf.Cos(i), 0, Mathf.Sin(i));
-            var scan = BBInterface.Scan(dir);
-        }
-        BBInterface.Move(direction);
-    }
+            var scan = BotInterface.Scan(dir);
 
+
+            if (scan.type == HitType.World)
+            {
+                direction = Vector3.Slerp(direction, -dir, 1 - (scan.distance / GameManager.instance.maxLookDistance));
+            }
+            else if (scan.type == HitType.Enemy)
+            {
+                BotInterface.Shoot(dir);
+                direction = scan.distance > 2f ? dir : -dir;
+
+                break;
+            }
+            else if (scan.type == HitType.Item)
+            {
+                pickupTarget = scan.pickup;
+
+                direction = (pickupTarget.transform.position - transform.position).normalized;
+                BotInterface.Pickup(pickupTarget.GetComponent<Pickup>());
+                if (BotInterface.health <= 75)
+                {
+                    BotInterface.UseItem(healingItem);
+                }
+                if (BotInterface.armor <= 75)
+                {
+                    BotInterface.UseItem(armorItem);
+                }
+
+            }
+
+        }
+        BotInterface.Move(direction);
+
+    }
 }
