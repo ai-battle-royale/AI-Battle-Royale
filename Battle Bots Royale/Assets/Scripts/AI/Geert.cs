1f0585ceda6177d72c22b256d930abf89c7dbb1d
GSjoerdstra <45204487+GSjoerdstra@users.noreply.github.com>
2019-03-15 19:26:41 +0100

    Update Geert.cs

--- Battle Bots Royale/Assets/Scripts/AI/Geert.cs
+++ Battle Bots Royale/Assets/Scripts/AI/Geert.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class Geert : MonoBehaviour
 {
-    BattleBotInterface Manager;
-
+    BattleBotInterface BBInterface;
+    private Vector3 direction;
     // Start is called before the first frame update
     void Start()
     {
-        Manager = GetComponent<BattleBotInterface>();
+        BBInterface = GetComponent<BattleBotInterface>();
+        direction = new Vector3(Random.value, 0, Random.value);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        for (var i = 0f; i < Mathf.PI * 2; i += Mathf.PI / 8)
+        {
+            var dir = new Vector3(Mathf.Cos(i), 0, Mathf.Sin(i));
+            var scan = BBInterface.Scan(dir);
+        }
+        BBInterface.Move(direction);
     }
 
 }
