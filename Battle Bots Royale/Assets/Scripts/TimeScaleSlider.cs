c32c63fa7581ed661dca7ba5133dffc9364474b6
Blieper <yoranmandema@eml.cc>
2019-03-29 21:39:12 +0100

    Timescale sniper

---
+++ Battle Bots Royale/Assets/Scripts/TimeScaleSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeScaleSlider : MonoBehaviour {
+
+    private Slider slider;
+
+    void Start() {
+        slider = GetComponent<Slider>();
+    }
+
+    void Update() {
+        Time.timeScale = slider.value;
+    }
+}
